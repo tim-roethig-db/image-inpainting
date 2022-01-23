3e084230a3a2b3b646f2548f200568411dcd2afd
Blumio <matzeblum@gmx.de>
2022-01-23 19:47:27 +0100

    EDIT: line mask algo rework for efficiency

---
+++ prep_data_efficient_lines.py
@@ -0,0 +1,115 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from skimage.draw import line, disk
+import os
+import glob
+import torch
+from torchvision import transforms
+#from tqdm import tqdm
+
+
+class PrepData(torch.utils.data.Dataset):
+    def __init__(self, n_samples):
+        super().__init__()
+
+        self.n_samples = n_samples
+        self.min_patch_size = 0.2
+        self.max_patch_size = 0.3
+
+        id = os.environ["SLURM_JOB_ID"]
+        #self.img_paths = glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/data/data_celeba/*.jpg')[:self.n_samples]
+        self.img_paths = glob.glob(f'/scratch/{id}' + '/data/data_celeba/*.jpg')[:self.n_samples]
+        self.num_imgs = len(self.img_paths)
+
+        self.img_transformer = transforms.ToTensor()
+        
+    def __getitem__(self, index):
+        """
+        Parameters to tweak:
+        --- maxLines: Maximal number of random lines
+        --- lines (low): Minimum number of random lines
+        --- lowRad: Minimum radius of circles drawn on lines
+        --- highRad: Maximum radius of circles drawn on lines
+        --- function: Determines pattern of circle sizes on one line
+        """
+        img = Image.open(self.img_paths[index]).resize(size=(256, 256))
+        img = self.img_transformer(img.convert('RGB'))
+        # Determine how many lines should be defined
+        # Can be tweaked:
+        maxLines = 25
+        lines = np.random.randint(1, maxLines)
+        lines *= 2
+        lowRad = 3
+        highRad = 16
+
+        # Init storing vectors
+        all_line_rows = []
+        all_line_cols = []
+        all_circle_rows = []
+        all_circle_cols = []
+        
+        # Mask init
+        mask = torch.ones(size=img.shape, dtype=torch.float64)
+
+        # Choose maximum radius of circles
+        maxRad = np.random.randint(lowRad, highRad)
+                
+        # Generate x and y coordinates for lines: x[even]=x_start, x[odd]=x_end
+        x = np.random.randint(1, img.shape[0]-1, size=lines)
+        y = np.random.randint(1, img.shape[1]-1, size=lines)
+        x = np.int_(x)
+        y = np.int_(y)
+
+        # Make lines
+        for i in range(lines):
+            if i % 2 == 0:
+                row, col = line(x[i], y[i], x[i+1], y[i+1])
+                # Store all line indices in these vectors
+                all_line_rows = all_line_rows.append(row)
+                all_line_cols = all_line_cols.append(col)
+
+        # Decide how big the radius of disks should be        
+        rand = np.random.randint(0, 1000, 100)
+
+        # Draw circles for every line coordinate
+        for i in range(len(all_line_rows)):
+            # Draw only every 6th circle
+            if i % 6 == 0:
+                # Let radius vary
+                function = maxRad*np.sin(rand[i])*2
+                upperBound = min(max(function, lowRad), maxRad)
+                radius = np.random.randint(lowRad-1, upperBound)
+
+                rowCirc, colCirc = disk((all_line_rows[i], all_line_cols[i]), 
+                                        radius, 
+                                        shape=(img.shape[0], img.shape[1]))
+                all_circle_rows = all_circle_rows.append(rowCirc)
+                all_circle_cols = all_circle_cols.append(colCirc)
+
+        mask[:, all_circle_rows, all_circle_cols] = 0
+        
+        img = torch.as_tensor(img, dtype=torch.float64)
+    
+        return (img * mask), mask, img
+
+if __name__ == '__main__':
+    # Save masks as tensor files (.pt) and load them later to decrease learning time
+    # for j in tqdm(range(1, 1001)):
+    #     mi, m, i = PrepData()[1]
+    #     torch.save(m, (os.getcwd() + f'\\masks\\mask_{j+1000}.pt'))
+    mi, m, i = PrepData()[1]
+    plt.imshow(mi.permute(1, 2, 0))
+    plt.show()
+    # print(mi.shape)
+    # print(mi.dtype)
+    # print(m.shape)
+    # print(m.dtype)
+    # print(i.shape)
+    # print(i.dtype)
+
+"""
+Features lost due to optimization of speed:
+ --- higher weighted probability for shorter lines
+ --- 
+"""
