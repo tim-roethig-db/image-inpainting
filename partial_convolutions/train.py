7db837dfe2acb5d2dc3a0fb0fbde08935ae85478
tro16 <tim.roethig@yahoo.de>
2022-04-05 00:23:20 +0200

    tests

--- partial_convolutions/train.py
+++ partial_convolutions/train.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     epochs = 4
     device = torch.device('cpu')
 
-    data_train = PrepData(n_samples=10)
+    data_train = PrepData(n_samples=batch_size * 2)
     print(f"Loaded training dataset with {data_train.num_imgs} samples")
 
     iters_per_epoch = data_train.num_imgs // batch_size
--- prep_data/prep_data.py
+++ prep_data/prep_data.py
@@ -14,12 +14,12 @@ class PrepData(torch.utils.data.Dataset):
         self.n_samples = n_samples
         self.min_patch_size = 0.2
         self.max_patch_size = 0.3
-        #print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/data_celeba/*.jpg')
+        print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/data_celeba/*.jpg')
         #self.img_paths = glob.glob(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/data_celeba/*.jpg')
         #self.img_paths = glob.glob('/home/n1124578/Image-Inpainting/data_celeba/*.jpg')
         self.img_paths = glob.glob('/Users/tim/Desktop/Master/Semester_3/ML_PRAK/Image-Inpainting/data_celeba/*.jpg')
         self.img_paths = self.img_paths[:self.n_samples]
-        #print(self.img_paths)
+        print(self.img_paths)
         self.num_imgs = len(self.img_paths)
 
         self.img_transformer = transforms.ToTensor()
