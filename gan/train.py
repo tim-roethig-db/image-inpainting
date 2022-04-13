1189dd2e4c28196a6a5c4ca7c1931691a375195c
tro16 <tim.roethig@yahoo.de>
2022-04-13 12:37:47 +0200

    tests

--- gan/train.py
+++ gan/train.py
@@ -7,14 +7,14 @@ from model import InpaintGenerator, Discriminator
 
 
 if __name__ == "__main__":
-    batch_size = 2
+    batch_size = 16
     lr = 1e-4
     epochs = 2
     beta1 = 0.5
     beta2 = 0.999
     device = torch.device('cpu')
 
-    data_train = PrepData(n_samples=batch_size * 4)
+    data_train = PrepData(n_samples=batch_size * 1000)
     print(f"Loaded training dataset with {data_train.num_imgs} samples")
 
     iters_per_epoch = data_train.num_imgs // batch_size
