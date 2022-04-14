1786af66d53bc3cb4edc0c4acce01c3a12bd850a
tim-roethig <tim.roethig@nordlb.de>
2022-04-14 15:42:07 +0200

    tests

--- gan/train.py
+++ gan/train.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
                 monitor_l1_loss = monitor_l1_loss / j
                 monitor_gen_loss = monitor_gen_loss / j
                 monitor_dis_loss = monitor_dis_loss / j
-                print(f"{i} l1: {round(monitor_l1_loss, 4)}, gen_los: {round(monitor_gen_loss, 4)}, dis_loss: {round(monitor_dis_loss, 4)}")
+                print(f"{i} l1: {torch.round(monitor_l1_loss, 4)}, gen_los: {torch.round(monitor_gen_loss, 4)}, dis_loss: {torch.round(monitor_dis_loss, 4)}")
                 monitor_l1_loss = 0
                 monitor_gen_loss = 0
                 monitor_dis_loss = 0
