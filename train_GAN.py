0ca402f861246926f76ccef3aa18d65c8b7b54e1
tro16 <tim.roethig@yahoo.de>
2022-08-22 11:34:30 +0200

    dw

--- train_GAN.py
+++ train_GAN.py
@@ -9,7 +9,7 @@ from model import InpaintGenerator, Discriminator
 
 
 if __name__ == "__main__":
-    batch_size = 32
+    batch_size = 24
     lr = 0.0001
     epochs = 1
     block_num = 4
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     data_train = PrepData(n_samples=n_samples)
     print(f"Loaded training dataset with {data_train.num_imgs} samples")
 
-    iters_per_epoch = (data_train.num_imgs - test_size) // batch_size
+    iters_per_epoch = (n_samples - test_size) // batch_size
 
     generator = InpaintGenerator(rates=[1, 2, 4, 8], block_num=block_num).float()
     generator = torch.nn.DataParallel(generator)
