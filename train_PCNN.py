ee4fb1774a39af9f62ad0afc8d31a39a7340f81d
tro16 <tim.roethig@yahoo.de>
2022-08-23 10:23:34 +0200

    dw

--- train_PCNN.py
+++ train_PCNN.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     lr = 0.01
     epochs = 1
     n_samples = 4910
-    test_size = 1010
+    test_size = 10
     j = 100
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
